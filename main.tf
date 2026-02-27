52c74548afb7b925b890bbedcd9f66932f279f54
Karan Patel <patel_karan_cool@hotmail.com>
2026-02-26 21:16:35 -0500

    Configure AWS provider in main.tf
    Add AWS provider configuration to Terraform file

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.34.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-2
+}
 resource "aws_instance" "name" {
   ami           = "ami-09256c524fab91d36"
   instance_type ="t3.micro"
