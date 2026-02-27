0534343522b64e8c21738d3a431f22864101ff58
Karan Patel <patel_karan_cool@hotmail.com>
2026-02-26 20:56:38 -0500

    Update AMI ID for AWS instance in main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
+provider "aws" {
+  region = "us-east-2"
+}
 resource "aws_instance" "name" {
-  ami           = ""
+  ami           = "ami-09256c524fab91d36"
   instance_type ="t3.micro"
   tags = {
     name = "HCP-SERVER"
